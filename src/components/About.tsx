d8fe112913f2ca62160f7555a47033501cfcf3f7
sijanthapaa34 <sijanmgr034@gmail.com>
2025-04-07 09:44:44 +0545

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -33,7 +33,7 @@ export default function About() {
             </Button>
             
             <a 
-              href="/lovable-uploads/cd20738c-5551-464d-b92b-50e68b9ae6e9.png" 
+              href="/lovable-uploads/CV Sijan Thapa.pdf" 
               target="_blank" 
               rel="noopener noreferrer"
               className="glass flex items-center gap-3 group hover:scale-105 transition-all duration-300 py-2 px-4 rounded-md"
